ec08fe13c106c7d3d7fdfb8d75135f737c34ee75
Oussama Nairi <nairioussama@live.fr>
2020-04-02 23:55:09 +0100

    create ajouter.php

---
+++ ajouter.php
@@ -0,0 +1,17 @@
+<div class="well">
+     <h1> Gestion des Livres</h1>
+</div>
+<p><a href="" class="btn btn-primary">Ajouter un Livre</a></p>
+<div class="col-md-6">
+   <form method="get" action="add.php">
+        <div class="form-group">
+         <label>Titre</lable>
+         <input type="text" class="form-control" name="titre" placeholder="Titre">
+        </div>
+         <div class="form-group">
+         <label>Quantite</lable>
+         <input type="text" class="form-control" name="qte" placeholder="Quantite">
+        </div>
+         <button type="submit" class="btn btn-default">Enregistrer</button>
+    </form>
+</div>
